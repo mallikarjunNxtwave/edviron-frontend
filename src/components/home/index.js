403bdd75370c76a51cfb22376ead3fc849ec8f75
mallikarjun <mallijangam@gmail.com>
2025-01-12 20:47:04 +0530

    adding after majarity work completed

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,27 +1,93 @@
 import { useEffect, useState } from 'react'
-// import cookies from 'js-cookie'
-import Navigate from 'react-router-dom'
+import Cookies from 'js-cookie'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navigate} from 'react-router-dom'
+import { FaSearch } from "react-icons/fa";
+import { HomeContainer, Heading, SearchBox, InputBox, FilteringContainer, TableHeading, SearchButton, TableContainer } from '../styledComponents'
 
-
+const apiStatuses = {
+    initial: 'INITIAL',
+    inprogress: 'inprogress',
+    success: 'SUCCESS',
+    failure: 'FAILURE'
+}
 
 const Home = () => {
+    const [apiStatus, setApiStatus] = useState(apiStatuses.initial)
+    const [customOrderId, setCustomOrderId] = useState('')
+    const [transactions, setTransactions] = useState([])
+    const [url, setUrl] = useState('https://edviron-backend-jh1s.onrender.com/transactions')
 
     const GetTransactions = async () => {
-        const url = 'https://edviron-backend-jh1s.onrender.com/transactions'
+        setApiStatus(apiStatuses.inprogress)
         const response = await fetch(url)
-        const data = await response.json()
-        console.log(data)
+        console.log(response)
+        if(response.ok){
+            const data = await response.json()
+            setTransactions(data.transactions)
+            setApiStatus(apiStatuses.success)
+        }else {
+            setApiStatus(apiStatuses.failure)
+        }
+    }
 
+    const handleStateChange = (event, setFunction) => {
+        setFunction(event.target.value)
     }
 
     useEffect(() => {
         GetTransactions();
     }, [])
 
+    const renderTransactions = () => (
+        <TableContainer className='table table-hover table-bordered'>
+            <thead>
+                <tr>
+                    <TableHeading scope='col'>Collect ID</TableHeading>
+                    <TableHeading scope='col'>School ID</TableHeading>
+                    <TableHeading scope='col'>Gateway</TableHeading>
+                    <TableHeading scope='col'>Order Amount</TableHeading>
+                    <TableHeading scope='col'>Transaction Amount</TableHeading>
+                    <TableHeading scope='col'>Status</TableHeading>
+                    <TableHeading scope='col'>Custom Order ID</TableHeading>
+                </tr>
+            </thead>
+            <tbody>
+            {
+             transactions.map(each => {
+                return <tr key={each.customOrderId}>
+                    <TableHeading>{each.collect_id}</TableHeading>
+                    <TableHeading>{each.school_id}</TableHeading>
+                    <TableHeading>{each.gateway}</TableHeading>
+                    <TableHeading>{each.order_amount}</TableHeading>
+                    <TableHeading>{each.transaction_amount}</TableHeading>
+                    <TableHeading>{each.status}</TableHeading>
+                    <TableHeading>{each.custom_order_id}</TableHeading>
+                </tr>
+             })
+            }
+            </tbody>
+        </TableContainer>
+    )
+
+    const jwtToken = Cookies.get('jwt_token')
+    if (jwtToken === undefined) {
+        return <Navigate to='/login' />
+    }
+
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <HomeContainer>
+            <Heading>Edviron</Heading>
+            <FilteringContainer>
+                <SearchBox>
+                    <SearchButton>
+                    <FaSearch style={{height: '20px'}}/>
+                    </SearchButton>
+                    <InputBox placeholder='Custom Order Id' value={customOrderId} onChange={(event) => handleStateChange(event, setCustomOrderId)} />
+                </SearchBox>
+            </FilteringContainer>
+            {renderTransactions()}
+        </HomeContainer>
     )
 }
 
--- src/components/styledComponents/index.js
+++ src/components/styledComponents/index.js
@@ -1,4 +1,4 @@
-import {styled} from 'styled-components'
+import styled from 'styled-components'
 
 export const MainContainer = styled.div`
   width: 100%;
@@ -53,4 +53,85 @@ font-size: 15px;
 font-weight: 500;
 margin-top: auto;
 color: #364ad0;
+`
+
+export const HomeContainer = styled.div`
+width: 100%;
+padding: 30px;
+box-sizing: border-box;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center; 
+`
+
+export const Heading = styled.h1`
+font-family: "Roboto";
+font-size: 30px;
+font-weight: bold;
+color: #000000;
+`
+
+export const FilteringContainer = styled.div`
+width: 100%;
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+`
+
+export const SearchBox = styled.div`
+width: 300px;
+height: 30px;
+display: flex;
+flex-direction: row;
+justify-content: flex-start;
+align-items: center;
+border: solid 1px #000000;
+`
+export const SearchButton = styled.button`
+width: 50px;
+height: 25px;
+background-color: transparent;
+border: none;
+
+`
+
+export const InputBox = styled.input`
+margin-left: 5px;
+padding-left: 5px;
+width: 100%;
+border: solid 1px;
+outline: none;
+`
+
+export const FilterContainer = styled.ul`
+border: solid 1px;
+padding: 5px;
+width: 50%;
+display: flex;
+margin-left: 30px;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+list-style: none;
+`
+
+export const ListItem = styled.li`
+`
+
+export const StatusValues = styled.button`
+font-size: 13px;
+color: #000000;
+border: none;
+background-color:rgb(188, 172, 172);
+text-decoration: ${props => (props.iaActive ? "underline" : "none")};
+`
+
+export const TableHeading =styled.th`
+
+font-size: 13px;
+`
+export const TableContainer = styled.table`
+margin-top: 30px;
 `
